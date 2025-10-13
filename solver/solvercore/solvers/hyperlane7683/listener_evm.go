7ebba3e7f379090ffa5b7df72f2c6dded014f918
Matt Carter <96356887+0xDegenDeveloper@users.noreply.github.com>
2025-10-13 14:27:40 -0400

    gocritic for loops

--- solver/solvercore/solvers/hyperlane7683/listener_evm.go
+++ solver/solvercore/solvers/hyperlane7683/listener_evm.go
@@ -163,8 +163,9 @@ func (l *evmListener) processBlockRange(ctx context.Context, fromBlock, toBlock 
 
 	// Group logs by block
 	byBlock := make(map[uint64][]ethtypes.Log)
-	for _, event := range logs {
-		byBlock[event.BlockNumber] = append(byBlock[event.BlockNumber], event)
+	for i := range logs {
+		event := &logs[i]
+		byBlock[event.BlockNumber] = append(byBlock[event.BlockNumber], *event)
 	}
 
 	// Process blocks in order
@@ -173,7 +174,8 @@ func (l *evmListener) processBlockRange(ctx context.Context, fromBlock, toBlock 
 		events := byBlock[b]
 
 		// Process each event in this block
-		for _, event := range events {
+		for i := range events {
+			logEvent := &events[i]
 			// Use generated binding to parse Open events
 			filterer, err := contracts.NewHyperlane7683Filterer(l.contractAddress, l.client)
 			if err != nil {
@@ -182,7 +184,7 @@ func (l *evmListener) processBlockRange(ctx context.Context, fromBlock, toBlock 
 			}
 
 			// Parse Open event
-			event, err := filterer.ParseOpen(event)
+			event, err := filterer.ParseOpen(*logEvent)
 			if err != nil {
 				fmt.Printf("❌ Failed to parse Open event: %v\n", err)
 				continue
